31588758ddce009d0cfb0134bc697e812e2d4cd6
zjbanya <3335931700qq.com>
2026-03-23 19:12:21 +0800

    未使用 通用视图 的代码

--- django_Web/polls/views.py
+++ django_Web/polls/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
+
+
 ################## python 语法
 # 关键字参数：实参必须显示指定形参名，形式 -- 形参名 = 
 # 
@@ -33,12 +37,32 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "你看到的问题的结果： %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("你要对问题进行投票 %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "你还没有选择哦。",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # 在成功处理完 POST 数据后，
+        # 始终返回一个 HttpResponseRedirect。
+        # 这样可以防止用户点击“后退”按钮时数据被重复提交。 
+        # reverse() 可以根据 URL 名字，让 Django 算出实际访问路径
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 
