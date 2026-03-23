77239d1dcb006cb6dccf12c7045935d8e7f19b6d
zjbanya <3335931700qq.com>
2026-03-23 21:51:45 +0800

    ch 7 end

--- django_Web/polls/admin.py
+++ django_Web/polls/admin.py
@@ -1,8 +1,30 @@
+from re import A
+
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+"""
+1. QuestionAdmin → 自定义 Question 模型在后台的显示和操作方式
+2. 不是创建数据库表 → 表是 Question 模型创建的
+3. fieldsets → 控制编辑页面的字段分组
+4. 注册时传给 admin.site.register → 后台使用这个自定义配置
+"""
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("数据信息", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"] # 过滤器
+
+
+admin.site.register(Question, QuestionAdmin)
+
 
 
 
--- django_Web/polls/models.py
+++ django_Web/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 # 投标应用：需要两个模型 (类，并用继承写法：(models.Model))
 # 问题 Question 
 # 选项 Choice
@@ -10,12 +11,18 @@ class Question(models.Model):
     # id = models.AutoField(primary_key=True)  这段语句默认是自动加的
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     question_text = models.CharField(max_length=200) # 返回字符串类型，限制长度为 200 
     pub_date = models.DateTimeField("date published") # 返回日期类型
 
+    
 
 class Choice(models.Model):
     def __str__(self):
