344e2194d7ed35e69b9e011073642b836223ce91
zjbanya <3335931700qq.com>
2026-03-25 18:53:14 +0800

    在 vite.config.js 中配置代理

--- django_Web/frontend/vite.config.ts
+++ django_Web/frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // 保持路径不变
       },
     },
   },
